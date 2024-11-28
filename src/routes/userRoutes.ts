a93d26a11cec12a9cc969ee48ebb9b6ebec8855b
Bagas Pratama <bagassss.com@gmail.com>
2024-11-28 23:44:52 +0700

    Update userRoutes.ts

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,12 +3,10 @@ import { Router } from "express";
 const router = Router();
 let users: { name: string; age: number }[] = [];
 
-// GET: Retrieve all users
 router.get("/data", (req, res) => {
   res.json(users);
 });
 
-// POST: Add new user
 router.post("/data", (req, res) => {
   const { name, age } = req.body;
   if (!name || !age) {
